72a2a4937d589c9a1ce6bf9e8640af86c9b97806
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2021-03-27 23:16:34 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -20,3 +20,24 @@ document.addEventListener("DOMContentLoaded", function() {
     loader = document.getElementById('loader');
     loadNow(1);
 });
+
+function openPage(evt, cityName) {
+    // Declare all variables
+    var i, tabcont, bio;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcont = document.getElementsByClassName("tabcont");
+    for (i = 0; i < tabcont.length; i++) {
+      tabcont[i].style.display = "none";
+    }
+  
+    // Get all elements with class="bio" and remove the class "active"
+    bio = document.getElementsByClassName("bio");
+    for (i = 0; i < bio.length; i++) {
+      bio[i].className = bio[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
